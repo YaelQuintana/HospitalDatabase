188a9f39e35122c9fbb9f8e4b237b1d10baf00eb
YaelQuintana <l20212116@tectijuana.edu.mx>
2023-11-09 18:42:25 -0800

    intento connect

--- connect.py
+++ connect.py
@@ -2,10 +2,16 @@ import psycopg2
 import jinja2
 
 # Connect to the database
-conn = psycopg2.connect(database="mydatabase", user="myuser", password="mypassword", host="localhost", port=5432)
+conn = psycopg2.connect(
+    database="hospitaldb",
+    user="cryms",
+    password="1234",
+    host="localhost",
+    port=5432
+)
 cursor = conn.cursor()
 
-# Query the database 
+# Query the database
 cursor.execute("SELECT name, status FROM staff")
 results = cursor.fetchall()
 
@@ -32,6 +38,8 @@ template = jinja2.Template("""
 
 html = template.render(data=results)
 
+# Print or save the HTML as needed
 print(html)
 
+# Close the database connection
 conn.close()
