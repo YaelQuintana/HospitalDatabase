988f234effae902e9eb1cc4bf74597e47886bbd1
YaelQuintana <l20212116@tectijuana.edu.mx>
2023-11-11 13:58:25 -0800

    bs

--- connect.py
+++ connect.py
@@ -1,5 +1,8 @@
+from flask import Flask, render_template
 import psycopg2
 import jinja2
+
+app = Flask(__name__)
 
 # Connect to the database
 conn = psycopg2.connect(
@@ -12,34 +15,47 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 # Query the database
-cursor.execute("SELECT name, status FROM staff")
-results = cursor.fetchall()
 
-# Pass data to HTML template
-template = jinja2.Template("""
-<!DOCTYPE html>
-<html>
-<body>
 
-  <h1>Staff Status</h1>
+@app.route("/", methods=["POST", "GET"])
+def display_table():
+    cursor.execute("SELECT name, status FROM staff")
+    results = cursor.fetchall()
+    return render_template("staff.html", data=results)
 
-  <table>
-    {% for row in data %}
-    <tr>
-      <td>{{row[0]}}</td>
-      <td>{{row[1]}}</td>
-    </tr>
-    {% endfor %}
-  </table>
-  
-</body>
-</html>
-""")
-
-html = template.render(data=results)
+## Pass data to HTML template
+#template = jinja2.Template("""
+#<!DOCTYPE html>
+#<html>
+#<body>
+#
+#  <h1>Staff Status</h1>
+#
+#  <table>
+#    {% for row in data %}
+#    <tr>
+#      <td>{{row[0]}}</td>
+#      <td>{{row[1]}}</td>
+#    </tr>
+#    {% endfor %}
+#  </table>
+#  
+#</body>
+#</html>
+#""")
+#
+#html = template.render(data=results)
 
 # Print or save the HTML as needed
-print(html)
+#print(html)
+
+#Display in application
+
+
+
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
 
 # Close the database connection
 conn.close()
