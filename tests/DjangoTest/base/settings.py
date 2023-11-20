3b1e796f4ed59d17ac4480a4b8a26e06ce421999
Misa <101288765+EndskendV@users.noreply.github.com>
2023-11-19 19:10:58 -0800

    Update settings.py

--- tests/DjangoTest/base/settings.py
+++ tests/DjangoTest/base/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "NAME": "hospitaladb",
         "USER": "hospitaladmin",
         "PASSWORD": "1234",
-        "HOST": "post4parra.crabdance.com", ##HOST ALTERNATIVO: "post4parra.ddns.net"
+        "HOST": "post4parra.ddns.net", # "post4parra.crabdance.com", ##HOST ALTERNATIVO: "post4parra.ddns.net"
         "PORT": "5432",
     }
 }
