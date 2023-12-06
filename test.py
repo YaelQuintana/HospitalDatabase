3679d730bb85cd8d511c834903d748b0d9afc380
YaelQuintana <l20212116@tectijuana.edu.mx>
2023-12-06 02:15:43 -0800

    cleaning

--- test.py
+++
@@ -1,8 +0,0 @@
-matriz = [[1, 2 ], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16], [17, 18],[19, 20], [21, 22]] 
-suma = 0 
-for x in range(len(matriz)): 
-    suma += x 
-    print(x)
-    
-print(suma)
-print(matriz[3])
